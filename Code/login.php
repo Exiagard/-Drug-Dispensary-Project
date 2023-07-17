f449bfa4644b9659699619d8db03afe6f409099a
Muga Karmi Mugiisa Otieno <exiagardofficial@gmail.com>
2023-07-17 10:46:16 +0300

    Code

--- Code/login.php
+++
@@ -1,4 +0,0 @@
-<a href="Doctor/DoctorHome.php">Doctor</a>
-<a href="Pharmacy/PharmacyHome.php">Pharmacy</a>
-<a href="PharmaceuticalCompany/PCHome.php">PharmaceuticalCompany</a>
-<a href="Patients/PatientHome.php">Patients</a>
