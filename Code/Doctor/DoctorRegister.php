9daaed3a12f837b4834b39a0cb951ede6c429810
Tiffany Jane Waithira <131330504+Twna-Jane@users.noreply.github.com>
2023-07-17 08:09:01 +0300

    Code

--- Code/Doctor/DoctorRegister.php
+++ Code/Doctor/DoctorRegister.php
@@ -4,6 +4,7 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Doctor Register</title>
+	<link rel="stylesheet" href="styles.css
 </head>
 <body>
 	<form action="DoctorInsert.php" method="POST">
