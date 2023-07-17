5db1042bf8cd797b48956d4526716e74572c87fa
Tiffany Jane Waithira <131330504+Twna-Jane@users.noreply.github.com>
2023-07-17 11:19:43 +0300

    Code

--- NewCode/Code/PharmacistView/DispenseDrugs.php
+++ NewCode/Code/PharmacistView/DispenseDrugs.php
@@ -52,6 +52,7 @@ if( $_SERVER['REQUEST_METHOD'] == 'POST'){
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title></title>
+	<link rel="stylesheet" href="dispensedrugs.css">
 </head>
 <body>
 	<center>
