2cfa35a3ec1c8d3c6dcef8bd78441435f04cc30e
Tiffany Jane Waithira <131330504+Twna-Jane@users.noreply.github.com>
2023-07-17 09:53:15 +0300

    Code

--- NewCode/Code/Patients/PatientRegister.php
+++ NewCode/Code/Patients/PatientRegister.php
@@ -4,6 +4,7 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Patient Register</title>
+	<link rel="stylesheet" href="patientregister.css">
 </head>
 <body>
 	<center>
