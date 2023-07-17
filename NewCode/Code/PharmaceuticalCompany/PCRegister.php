ae57be6b8d97bafda322c7bfa1fc1e8881a95c7c
Tiffany Jane Waithira <131330504+Twna-Jane@users.noreply.github.com>
2023-07-17 09:59:24 +0300

    Code

--- NewCode/Code/PharmaceuticalCompany/PCRegister.php
+++ NewCode/Code/PharmaceuticalCompany/PCRegister.php
@@ -4,6 +4,7 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Pharmaceutical Company Register</title>
+	<link rel="stylesheet" href="pcregister.css">
 </head>
 <body>
 	<center>
