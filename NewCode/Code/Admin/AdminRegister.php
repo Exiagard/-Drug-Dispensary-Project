2badd560032cac71afe94347d00798bddeefe16a
Tiffany Jane Waithira <131330504+Twna-Jane@users.noreply.github.com>
2023-07-17 18:19:02 +0300

    Code

--- NewCode/Code/Admin/AdminRegister.php
+++ NewCode/Code/Admin/AdminRegister.php
@@ -4,6 +4,7 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Admin Register</title>
+	<link rel="stylesheet" href="adminregister.css">
 </head>
 <body>
 	<center>
