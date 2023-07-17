74fb953a41e4bc9fcb5a4abfe3686809e087084d
Tiffany Jane Waithira <131330504+Twna-Jane@users.noreply.github.com>
2023-07-17 11:07:11 +0300

    Code

--- NewCode/Code/DoctorsView/PrescribeDrugs.php
+++ NewCode/Code/DoctorsView/PrescribeDrugs.php
@@ -50,6 +50,7 @@ if( $_SERVER['REQUEST_METHOD'] == 'POST'){
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title></title>
+	<link rel="stylesheet" href="prescribeddrug.css">
 </head>
 <body>
 	<center>
