0fd00cd49aca902b4f164218a5ce6195f55253b1
Tiffany Jane Waithira <131330504+Twna-Jane@users.noreply.github.com>
2023-07-18 12:10:22 +0300

    Code

--- NewCode/Code/PharmacistView/DispensedDrugsCRUD.php
+++ NewCode/Code/PharmacistView/DispensedDrugsCRUD.php
@@ -4,6 +4,7 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title></title>
+	<link rel="stylesheet" href="dispensedrugscrud.css">
 </head>
 <body>
 	<h2>Dispensed Drugs</h2>
